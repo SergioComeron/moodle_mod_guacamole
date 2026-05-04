7a838a45676d61a0729d61cffe592b65496c0bca
SergioComeron <sergiocomeron@icloud.com>
2026-05-04 10:10:29 +0200

    release: bump to v1.7 and update build date to 2026050401

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_guacamole';
-$plugin->version = 2024070103;
-$plugin->release = 'v1.6';
+$plugin->version = 2026050401;
+$plugin->release = 'v1.7';
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
